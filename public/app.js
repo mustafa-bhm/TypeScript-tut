285c769c67760d4f9908aac19838b5ac9180e356
mustafa <mustafa.bhm3@gmail.com>
2022-12-31 09:35:35 -0700

    commit

--- public/app.js
+++ public/app.js
@@ -13,12 +13,14 @@ const list = new ListTemplate(ul);
 //*** add event listener
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+    let values;
+    values = [tofrom.value, details.value, amount.valueAsNumber];
     let doc;
     if (type.value === "invoice") {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     }
     else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
     list.render(doc, type.value, "end");
 });
